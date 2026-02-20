dea8eb70d7d6a1485671ad092efe37fce51a57e0
Jakub Hanslik <jakub@hanslikovi.com>
2026-02-20 20:53:42 +0100

    Blink

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,11 @@
 return {
     "neovim/nvim-lspconfig",
+    dependencies = { 'saghen/blink.cmp' },
+    config = function()
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+        vim.lsp.config('pyright', { capabilities = capabilities })
+        vim.lsp.config('clangd', { capabilities = capabilities })
+
+    end
 }
