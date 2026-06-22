fa3dee8ce32f9d71bfb4304ac3dbde609d43f109
Jakub Hanslik <jakub@hanslikovi.com>
2026-06-22 20:12:10 +0200

    Transparent nvim

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,1 +1,9 @@
-vim.cmd[[colorscheme rose-pine]]
+function colorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+colorMyPencils()
+
